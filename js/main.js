93646cd0b53c93614e8e49f745f156e47bda23db
samandarsaburov <saburovsamandar@gmail.com>
2023-02-24 19:01:32 +0500

    done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const elTopList =  findElement("#products__top");
+const elTopList = findElement("#products__top");
 const elTopTemplate = findElement("#product-template")
 
 console.log(products);
@@ -7,30 +7,57 @@ products.slice(0, 20).forEach((products) => {
     const newProduct = elTopTemplate.content.cloneNode(true);
     // console.log(products);
 
-    const elTitle = findElement("#title",newProduct);
-    const elPrice = findElement("#price",newProduct);
-    const elRating = findElement("#rating",newProduct);
-    const elImg = findElement("img",newProduct);
+    const elTitle = findElement("#title", newProduct);
+    const elPrice = findElement("#price", newProduct);
+    const elRating = findElement("#rating", newProduct);
+    const elImg = findElement("img", newProduct);
 
     elTitle.textContent = products.name;
     elPrice.textContent = products.price + "$";
-    elRating.textContent =   products.rating + "rating";
+    elRating.textContent = products.rating + "rating";
     elImg.src = products.image;
 
 
-    
+
     elTopList.appendChild(newProduct);
-}) 
+})
+
+//---------------------- language----------------------
 
 
-const lang = localStorage.getItem("lang");
 
-console.log(lang);
+const ellangSelect = findElement("#language-select")
+
+let lang = localStorage.getItem("lang");
+
+ellangSelect.value = lang
 
 if (lang == 'uz') {
     document.title = "internet do'kon";
-} else if (lang == 'eng') {
+} else if (lang == 'en') {
     document.title = "E-commerce";
+} else if (lang == 'ru') {
+    document.title = "интернет магазин";
 }
 
-localStorage.setItem("lang", "eng")
+
+ellangSelect.addEventListener("change", () => {
+
+    const value = ellangSelect.value;
+
+    localStorage.setItem("lang", value);
+
+    lang = value
+
+    if (lang == 'uz') {
+        document.title = "internet do'kon";
+    } else if (lang == 'en') {
+        document.title = "E-commerce";
+    } else if (lang == 'ru') {
+        document.title = "интернет магазин";
+    }
+})
+
+
+
+
