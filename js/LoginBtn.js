5696dbce063703df5172ddb7e7dcc43924612681
samandarsaburov <saburovsamandar@gmail.com>
2023-02-28 15:52:48 +0500

    buttons worked

---
+++ js/LoginBtn.js
@@ -0,0 +1,11 @@
+const elLoginBtn = findElement('#login-btn');
+
+elLoginBtn.addEventLIstener('click', () => {
+    console.log('çlicked');
+} );
+
+
+function LoginHandler(evt) {
+    
+    console.log('çlicked 2');
+}
